b777c629bd21739ce2714d52f1002d3f1c7d0add
Andrew Kane <acekane1@gmail.com>
2016-10-02 15:29:47 -0700

    Revert attempts to fix

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,17 @@
 require "bundler/setup"
-
+Bundler.require
 require "rails/all"
-
-Bundler.require(*Rails.groups)
 
 module PgHeroSolo
   class Application < Rails::Application
+    routes.append do
+      mount PgHero::Engine, at: "/"
+    end
+
     config.cache_classes = true
     config.eager_load = true
     config.log_level = :info
     config.secret_key_base = ENV["SECRET_KEY_BASE"] || SecureRandom.hex(30)
-    config.assets.js_compressor = :uglifier
 
     PgHero.show_migrations = false
   end

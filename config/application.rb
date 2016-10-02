f99a06dc3d1e2fa0425ff7da9ed8ec2fc601c9a1
Andrew Kane <acekane1@gmail.com>
2016-10-02 16:33:18 -0700

    Use disabled

--- config/application.rb
+++ config/application.rb
@@ -13,9 +13,9 @@ module PgHeroSolo
     config.log_level = :info
     config.secret_key_base = ENV["SECRET_KEY_BASE"] || SecureRandom.hex(30)
 
-    config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"] != "false"
+    config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"] != "disabled"
 
-    if ENV["RAILS_LOG_TO_STDOUT"] != "false"
+    if ENV["RAILS_LOG_TO_STDOUT"] != "disabled"
       logger           = ActiveSupport::Logger.new(STDOUT)
       logger.formatter = config.log_formatter
       config.logger = ActiveSupport::TaggedLogging.new(logger)

febf01d66353372aff6b0a93ed34242e4aaa8feb
Andrew Kane <acekane1@gmail.com>
2016-10-02 16:21:10 -0700

    Do not log to stdout in package

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,14 @@ module PgHeroSolo
     config.log_level = :info
     config.secret_key_base = ENV["SECRET_KEY_BASE"] || SecureRandom.hex(30)
 
+    config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"] != "false"
+
+    if ENV["RAILS_LOG_TO_STDOUT"] != "false"
+      logger           = ActiveSupport::Logger.new(STDOUT)
+      logger.formatter = config.log_formatter
+      config.logger = ActiveSupport::TaggedLogging.new(logger)
+    end
+
     PgHero.show_migrations = false
   end
 end

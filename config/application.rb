605ae830fa5f2c920dbf596e62848c652a5bb7a4
Andrew Kane <acekane1@gmail.com>
2016-10-02 15:01:27 -0700

    Attempt to fix assets error

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,16 @@
 require "bundler/setup"
+
 require "rails/all"
+
 Bundler.require(*Rails.groups)
 
 module PgHeroSolo
   class Application < Rails::Application
-    routes.append do
-      mount PgHero::Engine, at: "/"
-    end
-
     config.cache_classes = true
     config.eager_load = true
     config.log_level = :info
     config.secret_key_base = ENV["SECRET_KEY_BASE"] || SecureRandom.hex(30)
+    config.assets.js_compressor = :uglifier
 
     PgHero.show_migrations = false
   end

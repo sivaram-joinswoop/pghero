cc821c91cbc715a9752d24fd339e8e0816c60b94
Andrew Kane <acekane1@gmail.com>
2017-05-12 20:06:30 -0700

    Added ability to specify bind address

--- config/puma.rb
+++ config/puma.rb
@@ -3,8 +3,12 @@ threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
 
 preload_app!
 
-rackup      DefaultRackup
-port        ENV['PORT']     || 3000
+rackup DefaultRackup
+if ENV['BIND']
+  bind ENV['BIND']
+else
+  port ENV['PORT'] || 3000
+end
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
